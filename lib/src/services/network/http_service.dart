034af6dbc1898e02eaec711ea8b343ae91d86775
Marco Fernandes <marcoaferc@gmail.com>
2023-09-20 19:55:38 -0300

    Build http interface

---
+++ lib/src/services/network/http_service.dart
@@ -0,0 +1,6 @@
+import 'package:cw_weather/src/core/exceptions/failure.dart';
+import 'package:cw_weather/src/services/network/response/base_response.dart';
+
+abstract interface class HttpService{
+  Future<(Failure?, BaseResponse?)> get({required String path, Map<String, dynamic>? queryParameters});
+}

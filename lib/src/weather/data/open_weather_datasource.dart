31425c4856b5f697cb73730f7fe84f69ebf44f09
Marco Fernandes <marcoaferc@gmail.com>
2023-09-20 21:25:49 -0300

    Implement datasource and tests

--- lib/src/weather/data/open_weather_datasource.dart
+++ lib/src/weather/data/open_weather_datasource.dart
@@ -1,20 +1,45 @@
 import 'package:cw_weather/src/core/exceptions/failure.dart';
-import 'package:cw_weather/src/services/network/impl/dio_service.dart';
+import 'package:cw_weather/src/core/network/http_service.dart';
+import 'package:cw_weather/src/weather/entities/weather.dart';
+import 'package:cw_weather/src/weather/models/weather_model.dart';
 
 class OpenWeatherDatasource{
-  final DioService dio;
+  final HttpService http;
 
-  OpenWeatherDatasource({required this.dio});
+  OpenWeatherDatasource({required this.http});
   
-  Future<(Failure?, dynamic)> getCurrentWeather({required String latitude, required String longitude})async{
-    return await dio.get(path: '/data/2.5/weather');
+  Future<(Failure?, Weather?)> getWeather({required String latitude, required String longitude})async{
+    try{
+      var response = await http.get(path: '/data/2.5/weather', queryParameters: {'lat': latitude, 'lon': longitude, 'units': 'metric'});
+      if(response.$1 != null){
+        // check if an error has ocurred
+        return (response.$1, null);
+      }
+      else{
+        return (null, WeatherModel.fromMap(response.$2?.data as Map<String, dynamic>));
+      }
+    } catch(e){
+      return (Failure.generic, null);
+    } 
   }
 
   Future<dynamic> getCoordinatesByName({required String query})async {
-    return await dio.get(path: '/geo/1.0/direct', queryParameters: {'q': query});
+    return await http.get(path: '/geo/1.0/direct', queryParameters: {'q': query});
   }
 
-  Future<dynamic> getForecast({required String latitude, required String longitude})async {
-    return await dio.get(path: '/data/2.5/forecast');
+  Future<(Failure?, List<Weather>?)> getForecast({required String latitude, required String longitude})async{
+    try{
+      var response = await http.get(path: '/data/2.5/forecast', queryParameters: {'lat': latitude, 'lon': longitude, 'units': 'metric'});
+      if(response.$1 != null){
+        // check if an error has ocurred
+        return (response.$1, null);
+      }
+      else{
+        var list = (response.$2!.data['list'] as List).map((e) => WeatherModel.fromMap(e as Map<String, dynamic>)).toList();
+        return (null, list);
+      }
+    } catch(e){
+      return (Failure.generic, null);
+    } 
   }
 }
--- test/data/open_weather_datasource_test.dart
+++ test/data/open_weather_datasource_test.dart
@@ -1,21 +1,254 @@
+import 'package:cw_weather/src/core/exceptions/failure.dart';
+import 'package:cw_weather/src/core/network/http_service.dart';
+import 'package:cw_weather/src/core/network/response/base_response.dart';
 import 'package:cw_weather/src/weather/data/open_weather_datasource.dart';
-import 'package:cw_weather/src/services/network/impl/dio_service.dart';
+import 'package:cw_weather/src/weather/models/weather_model.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockHttp extends Mock implements HttpService {}
 
 void main() {
-  group('Open weather datasource tests', () {
-
-    late DioService dio;
+  group('Open weather datasource', () {
+    late HttpService http;
     late OpenWeatherDatasource datasource;
-
-    setUpAll((){
-      dio = DioService();
-      datasource = OpenWeatherDatasource(dio: dio);
+    late Map<String, dynamic> weatherJson;
+    late Map<String, dynamic> forecastJson;
+    setUpAll(() {
+      http = MockHttp();
+      datasource = OpenWeatherDatasource(http: http);
     });
 
-    test('Test if data is received properly', () async {
-      var data = await datasource.getCoordinatesByName(query: 'Natal');
-      print(data);
+    test('Get current weather returns a valid model for valid data', () async {
+      when(
+        () => http.get(
+            path: '/data/2.5/weather',
+            queryParameters: {'lat': '1', 'lon': '2', 'units': 'metric'}),
+      ).thenAnswer(
+          (invocation) async => (null, BaseResponse(data: weatherJson)));
+
+      var data =
+          await datasource.getWeather(latitude: '1', longitude: '2');
+      expect(data.$1, null);
+      expect(data.$2, isA<WeatherModel>());
+      expect(data.$2!.weather, "Rain");
+      expect(data.$2!.temperature, 298.48);
+      expect(data.$2!.id, 501);
+      expect(data.$2!.description, 'moderate rain');
+      expect(data.$2!.icon, '10d');
     });
+
+    test('Get current weather returns failure for invalid data', () async {
+      when(
+        () => http.get(
+            path: '/data/2.5/weather',
+            queryParameters: {'lat': '1', 'lon': '2', 'units': 'metric'}),
+      ).thenAnswer((invocation) async => (null, BaseResponse(data: {})));
+
+      var data =
+          await datasource.getWeather(latitude: '1', longitude: '2');
+      expect(data.$1, isA<Failure>());
+      expect(data.$2, null);
+    });
+
+    test('Get forecast returns a valid model for valid data', () async {
+      when(
+        () => http.get(
+            path: '/data/2.5/forecast',
+            queryParameters: {'lat': '1', 'lon': '2', 'units': 'metric'}),
+      ).thenAnswer(
+          (invocation) async => (null, BaseResponse(data: forecastJson)));
+
+      var data = await datasource.getForecast(latitude: '1', longitude: '2');
+      expect(data.$1, null);
+      expect(data.$2, isA<List<WeatherModel>>());
+      expect(data.$2!.length, 4);
+      expect(data.$2!.first.id, 500);
+      
+    });
+
+    test('Get forecast returns failure for invalid data', () async {
+      when(
+        () => http.get(
+            path: '/data/2.5/forecast',
+            queryParameters: {'lat': '1', 'lon': '2', 'units': 'metric'}),
+      ).thenAnswer((invocation) async => (null, BaseResponse(data: {})));
+
+      var data = await datasource.getForecast(latitude: '1', longitude: '2');
+      expect(data.$1, isA<Failure>());
+      expect(data.$2, null);
+    });
+
+    weatherJson = {
+      "coord": {"lon": 10.99, "lat": 44.34},
+      "weather": [
+        {
+          "id": 501,
+          "main": "Rain",
+          "description": "moderate rain",
+          "icon": "10d"
+        }
+      ],
+      "base": "stations",
+      "main": {
+        "temp": 298.48,
+        "feels_like": 298.74,
+        "temp_min": 297.56,
+        "temp_max": 300.05,
+        "pressure": 1015,
+        "humidity": 64,
+        "sea_level": 1015,
+        "grnd_level": 933
+      },
+      "visibility": 10000,
+      "wind": {"speed": 0.62, "deg": 349, "gust": 1.18},
+      "rain": {"1h": 3.16},
+      "clouds": {"all": 100},
+      "dt": 1661870592,
+      "sys": {
+        "type": 2,
+        "id": 2075663,
+        "country": "IT",
+        "sunrise": 1661834187,
+        "sunset": 1661882248
+      },
+      "timezone": 7200,
+      "id": 3163858,
+      "name": "Zocca",
+      "cod": 200
+    };
+
+    forecastJson = {
+      "cod": "200",
+      "message": 0,
+      "cnt": 40,
+      "list": [
+        {
+          "dt": 1661871600,
+          "main": {
+            "temp": 296.76,
+            "feels_like": 296.98,
+            "temp_min": 296.76,
+            "temp_max": 297.87,
+            "pressure": 1015,
+            "sea_level": 1015,
+            "grnd_level": 933,
+            "humidity": 69,
+            "temp_kf": -1.11
+          },
+          "weather": [
+            {
+              "id": 500,
+              "main": "Rain",
+              "description": "light rain",
+              "icon": "10d"
+            }
+          ],
+          "clouds": {"all": 100},
+          "wind": {"speed": 0.62, "deg": 349, "gust": 1.18},
+          "visibility": 10000,
+          "pop": 0.32,
+          "rain": {"3h": 0.26},
+          "sys": {"pod": "d"},
+          "dt_txt": "2022-08-30 15:00:00"
+        },
+        {
+          "dt": 1661882400,
+          "main": {
+            "temp": 295.45,
+            "feels_like": 295.59,
+            "temp_min": 292.84,
+            "temp_max": 295.45,
+            "pressure": 1015,
+            "sea_level": 1015,
+            "grnd_level": 931,
+            "humidity": 71,
+            "temp_kf": 2.61
+          },
+          "weather": [
+            {
+              "id": 500,
+              "main": "Rain",
+              "description": "light rain",
+              "icon": "10n"
+            }
+          ],
+          "clouds": {"all": 96},
+          "wind": {"speed": 1.97, "deg": 157, "gust": 3.39},
+          "visibility": 10000,
+          "pop": 0.33,
+          "rain": {"3h": 0.57},
+          "sys": {"pod": "n"},
+          "dt_txt": "2022-08-30 18:00:00"
+        },
+        {
+          "dt": 1661893200,
+          "main": {
+            "temp": 292.46,
+            "feels_like": 292.54,
+            "temp_min": 290.31,
+            "temp_max": 292.46,
+            "pressure": 1015,
+            "sea_level": 1015,
+            "grnd_level": 931,
+            "humidity": 80,
+            "temp_kf": 2.15
+          },
+          "weather": [
+            {
+              "id": 500,
+              "main": "Rain",
+              "description": "light rain",
+              "icon": "10n"
+            }
+          ],
+          "clouds": {"all": 68},
+          "wind": {"speed": 2.66, "deg": 210, "gust": 3.58},
+          "visibility": 10000,
+          "pop": 0.7,
+          "rain": {"3h": 0.49},
+          "sys": {"pod": "n"},
+          "dt_txt": "2022-08-30 21:00:00"
+        },
+        {
+          "dt": 1662292800,
+          "main": {
+            "temp": 294.93,
+            "feels_like": 294.83,
+            "temp_min": 294.93,
+            "temp_max": 294.93,
+            "pressure": 1018,
+            "sea_level": 1018,
+            "grnd_level": 935,
+            "humidity": 64,
+            "temp_kf": 0
+          },
+          "weather": [
+            {
+              "id": 804,
+              "main": "Clouds",
+              "description": "overcast clouds",
+              "icon": "04d"
+            }
+          ],
+          "clouds": {"all": 88},
+          "wind": {"speed": 1.14, "deg": 17, "gust": 1.57},
+          "visibility": 10000,
+          "pop": 0,
+          "sys": {"pod": "d"},
+          "dt_txt": "2022-09-04 12:00:00"
+        }
+      ],
+      "city": {
+        "id": 3163858,
+        "name": "Zocca",
+        "coord": {"lat": 44.34, "lon": 10.99},
+        "country": "IT",
+        "population": 4593,
+        "timezone": 7200,
+        "sunrise": 1661834187,
+        "sunset": 1661882248
+      }
+    };
   });
 }

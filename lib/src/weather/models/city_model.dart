5299367938a28a283a49526f3fa8e0d3e0bc1512
Marco Fernandes <marcoaferc@gmail.com>
2023-09-21 19:24:18 -0300

    Build geocoding functions and model

---
+++ lib/src/weather/models/city_model.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:cw_weather/src/weather/view_model/entities/city.dart';
+
+class CityModel extends City {
+  CityModel(
+      {required super.latitude,
+      required super.longitude,
+      required super.name,
+      required super.countryCode,
+      required super.state});
+
+  factory CityModel.fromMap(Map<String, dynamic> map) {
+    return CityModel(
+      latitude: map['lat'] as double,
+      longitude: map['lon'] as double,
+      name: map['name'] as String,
+      countryCode: map['country'] as String,
+      state: map['state'] as String,
+    );
+  }
+
+  factory CityModel.fromJson(String source) =>
+      CityModel.fromMap(json.decode(source) as Map<String, dynamic>);
+}

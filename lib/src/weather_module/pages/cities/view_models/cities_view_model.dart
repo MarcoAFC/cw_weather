a67f2f12966ead73116da95315db701fabae06cd
Marco Fernandes <marcoaferc@gmail.com>
2023-09-22 17:58:52 -0300

    Finish citiesviewmodel

--- lib/src/weather_module/pages/cities/view_models/cities_view_model.dart
+++ lib/src/weather_module/pages/cities/view_models/cities_view_model.dart
@@ -1,22 +1,29 @@
-import 'package:cw_weather/src/weather_module/data/datasources/open_weather_datasource.dart';
-import 'package:cw_weather/src/weather_module/entities/city.dart';
 import 'package:flutter/material.dart';
 
-class CitiesViewModel{
-  final OpenWeatherDatasource datasource;
-  final ValueNotifier<List<City>> citiesNotifier;
+import 'package:cw_weather/src/core/exceptions/failure.dart';
+import 'package:cw_weather/src/weather_module/data/datasources/open_weather_datasource.dart';
+import 'package:cw_weather/src/weather_module/entities/city.dart';
 
-  CitiesViewModel({required this.datasource, required this.citiesNotifier});
+class CitiesViewModel {
+  final OpenWeatherDatasource datasource;
+  final ValueNotifier<List<City>?> citiesNotifier;
+  final ValueNotifier<Failure?> errorNotifier;
+
+  CitiesViewModel({
+    required this.datasource,
+    required this.citiesNotifier,
+    required this.errorNotifier,
+  });
 
 
   Future<void> fetchData(String query) async {
     var data = await datasource.getCoordinatesByName(query: query);
 
     if(data.$2 != null){
-      citiesNotifier.value = data.$2!;
+      citiesNotifier.value = data.$2;
     }
     else if(data.$1 != null){
-      //TODO: handle error
+      errorNotifier.value = data.$1;
     }
   }
 

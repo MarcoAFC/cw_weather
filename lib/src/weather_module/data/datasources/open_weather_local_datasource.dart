f02905613e4766f558d9810009b2e0949933e85b
Marco Fernandes <marcoaferc@gmail.com>
2023-09-24 13:05:52 -0300

    split local and remote datasources

---
+++ lib/src/weather_module/data/datasources/open_weather_local_datasource.dart
@@ -0,0 +1,50 @@
+import 'package:cw_weather/src/core/exceptions/failure.dart';
+import 'package:cw_weather/src/core/local_storage/local_storage_service.dart';
+import 'package:cw_weather/src/weather_module/data/models/city_model.dart';
+import 'package:cw_weather/src/weather_module/data/models/weather_model.dart';
+import 'package:cw_weather/src/weather_module/domain/entities/city.dart';
+import 'package:cw_weather/src/weather_module/domain/entities/weather.dart';
+
+class OpenWeatherLocalDatasource {
+  final LocalStorageService storage;
+
+  OpenWeatherLocalDatasource({required this.storage});
+
+  Future<(Failure?, List<City>?)> getAllCities() async {
+    try {
+      var data = await storage.getAll();
+
+      var list = data
+          .map((e) => CityModel.fromMap(e.value as Map<String, dynamic>))
+          .toList();
+      return (null, list);
+    } catch (e) {
+      return (Failure.generic, null);
+    }
+  }
+
+  Future<(Failure?, Weather?)> getWeather({required String cityId}) async {
+    try {
+      var data = await storage.readKey(key: cityId);
+
+      var list = WeatherModel.fromMap(data['weather']);
+      return (null, list);
+    } catch (e) {
+      return (Failure.generic, null);
+    }
+  }
+
+  Future<(Failure?, List<Weather>?)> getForecast(
+      {required String cityId}) async {
+    try {
+      var data = await storage.readKey(key: cityId);
+
+      var list = data['forecast']
+          .map((e) => CityModel.fromMap(e.value as Map<String, dynamic>))
+          .toList();
+      return list;
+    } catch (e) {
+      return (Failure.generic, null);
+    }
+  }
+}

94e62417a299305a1f243e6ceb4b5a83bf8ed35d
Marco Fernandes <marcoaferc@gmail.com>
2023-09-24 14:57:08 -0300

    add repository dependency

--- lib/src/app_dependencies.dart
+++ lib/src/app_dependencies.dart
@@ -4,6 +4,8 @@ import 'package:cw_weather/src/core/network/http_service.dart';
 import 'package:cw_weather/src/core/network/impl/dio_service.dart';
 import 'package:cw_weather/src/weather_module/data/datasources/open_weather_local_datasource.dart';
 import 'package:cw_weather/src/weather_module/data/datasources/open_weather_remote_datasource.dart';
+import 'package:cw_weather/src/weather_module/data/repositories/open_weather_repository_impl.dart';
+import 'package:cw_weather/src/weather_module/domain/repositories/open_weather_repository.dart';
 import 'package:cw_weather/src/weather_module/pages/cities/view_models/cities_view_model.dart';
 import 'package:cw_weather/src/weather_module/pages/weather/view_models/forecast_view_model.dart';
 import 'package:cw_weather/src/weather_module/pages/weather/view_models/weather_view_model.dart';
@@ -28,6 +30,7 @@ class DependencyHandler{
     di.registerLazySingleton<LocalStorageService>(() => HiveService());
     di.registerLazySingleton<OpenWeatherRemoteDatasource>(() => OpenWeatherRemoteDatasource(http: di.get()));
     di.registerLazySingleton<OpenWeatherLocalDatasource>(() => OpenWeatherLocalDatasource(storage: di.get()));
+    di.registerLazySingleton<OpenWeatherRepository>(() => OpenWeatherRepositoryImpl(local: di.get(), remote: di.get()));
     di.registerLazySingleton<CitiesViewModel>(() => CitiesViewModel(repository: di.get()));
     di.registerLazySingleton<WeatherViewModel>(() => WeatherViewModel(repository: di.get()));
     di.registerLazySingleton<ForecastViewModel>(() => ForecastViewModel(repository: di.get()));
